76c5b82e5fcc935b55d632686139d60e8ab683bd
conorhenley <cjhkb8@gmail.com>
2014-08-28 13:25:47 -0700

    addition of reprojection function to spatialtoolz

--- spatialtoolz.py
+++ spatialtoolz.py
@@ -2,6 +2,7 @@ import psycopg2
 import pandas as pd, numpy as np
 import pandas.io.sql as sql
 import urbansim.sim.simulation as sim
+from utils import DataLoader
 
 def tag(target_table_name, target_field, source_table_name, source_table_field, how='point_in_poly', target_df=None):
     """
@@ -91,3 +92,37 @@ def db_to_df(query):
     """Executes SQL query and returns DataFrame."""
     conn = sim.get_injectable('conn')
     return sql.read_frame(query, conn)
+    
+def reproject(target_table, geometry_column='geom', config_dir, new_table=None):
+    """
+    Reprojects target table into the srid specified in the project config
+
+    Parameters
+    ----------
+    target_table: str
+        Name of table to reproject.  Default is in-place reprojection.
+    geometry_column : str
+        Name of the geometry column in the target table. Default is 'geom'.
+    config_dir : str
+        Path to the directory where the project config is stored.
+    source_field : str
+        Name of field in source_table that contains the information.
+    new_table: str, optional
+        If new_table is specified, a copy of target table is made with name new_table
+    
+    Returns
+    -------
+    None : None
+        Target table's geometry column is reprojected to the SRID found in the config file.
+        Function detects current target table SRID and project SRID and converts on the database.
+
+    """
+    project_srid = str(DataLoader(config_dir).srid)
+    table_srid = str(st.get_srid(target_table, geometry_column))
+    if new_table:
+        st.exec_sql("CREATE TABLE %s as SELECT * FROM %s" % (new_table, target_table))
+        st.exec_sql("SELECT UpdateGeometrySRID('%s', '%s', %s)" % (new_table, geometry_column, project_srid))
+        st.exec_sql("UPDATE %s SET %s = ST_TRANSFORM(ST_SetSRID(%s, %s), %s)" % (new_table, geometry_column, geometry_column, table_srid, project_srid))
+    else:
+        st.exec_sql("SELECT UpdateGeometrySRID('%s', '%s', %s)" % (target_table, geometry_column, project_srid))
+        st.exec_sql("UPDATE %s SET %s = ST_TRANSFORM(ST_SetSRID(%s, %s), %s)" % (target_table, geometry_column, geometry_column, table_srid, project_srid))
