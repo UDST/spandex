e403ba86ac1cc8354476c57f1fef0bd1a11bb4ea
unknown <eddie.janowicz@gmail.com>
2014-08-28 09:08:07 -0700

    Adding file for spatial functions.  Currently just contains basic tag function

---
+++ spatialtoolz.py
@@ -0,0 +1,87 @@
+import psycopg2
+import pandas as pd, numpy as np
+import pandas.io.sql as sql
+import urbansim.sim.simulation as sim
+
+def tag(target_table_name, target_field, source_table_name, source_table_field, how='point_in_poly', target_df=None):
+    """
+    Tags target table with attribute of another table based on spatial relationship.
+
+    Parameters
+    ----------
+    target_table_name : str
+        Name of table to be tagged.  New field will be added, or existing field updated.
+    target_field : str
+        Name of field in target_table to add (if doesn't exist) or update (if exists).
+    source_table_name : str
+        Name of table containing information to tag target_table with.
+    source_field : str
+        Name of field in source_table that contains the information.
+    how : str, optional
+        How to relate the two tables spatially.  If not specified, defaults to 'point_in_poly'
+    target_df : DataFrame, optional
+        DataFrame to update based on the tagging operation.
+
+    Returns
+    -------
+    None : None
+        Field is added or updated on the target_table in the database, and returns nothing.
+        Unless target_df argument is used, in which case return value is DataFrame with the
+        new/updated column.
+
+    """
+    
+    if srid_equality(target_table_name, source_table_name) == False:
+        raise Exception('Projections are different')
+        
+    if db_col_exists(target_table_name, target_field) == False:
+        add_integer_field(target_table_name, target_field)
+        
+    if how == 'point_in_poly':
+        if db_col_exists(target_table_name, 'centroid') == True:
+            exec_sql("update %s set %s = b.%s from %s b where st_within(%s.centroid,b.geom)" % 
+                                        (target_table_name, target_field, source_table_field, source_table_name, target_table_name)) 
+        else:
+            exec_sql("update %s set %s = b.%s from %s b where st_within(ST_centroid(%s.geom),b.geom)" % 
+                                        (target_table_name, target_field, source_table_field, source_table_name, target_table_name))
+    
+    if target_df:
+        target_df_idx_name = target_df.index.name
+        new_col = db_to_df("select %s, %s from %s" % (target_df_idx_name, target_field, target_table_name)).set_index(target_df_idx_name)[target_field]
+        target_df[target_field] = new_col
+        return target_df
+    
+def get_srid(table_name, field):
+    try:
+        return db_to_df("SELECT FIND_SRID('public', '%s', '%s')" % (table_name, field)).values[0][0]
+    except:
+        Pass
+    
+def srid_equality(target_table_name, source_table_name):
+    srids = []
+    def check_append_srid(table_name, field_name):
+        if db_col_exists(table_name, field_name):
+            srids.append(get_srid(table_name,field_name))
+    check_append_srid(target_table_name, 'geom')
+    check_append_srid(source_table_name, 'geom')
+    check_append_srid(target_table_name, 'centroid')
+    check_append_srid(source_table_name, 'centroid')
+    srids = np.unique(srids)
+    return False if len(srids[srids>0]) > 1 else True  
+    
+def db_col_exists(table_name, column_name):
+    test = db_to_df("SELECT column_name FROM information_schema.columns WHERE table_name='%s' and column_name='%s';"%(table_name,column_name))
+    return True if len(test) > 0 else False
+
+def add_integer_field(table_name, field_to_add):
+    exec_sql("alter table %s add %s integer default 0;" % (table_name, field_to_add))
+
+def exec_sql(query):
+    cur = sim.get_injectable('cur')
+    conn = sim.get_injectable('conn')
+    cur.execute(query)
+    conn.commit()
+    
+def db_to_df(query):
+    conn = sim.get_injectable('conn')
+    return sql.read_frame(query, conn)
