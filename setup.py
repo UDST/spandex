06a6cfb4fa560c122066061c7980756d69b3fa7b
Dara Adib <dadib@synthicity.com>
2014-09-15 16:27:20 -0700

    Add GeoAlchemy and SQLAlchemy to setup.py

--- setup.py
+++ setup.py
@@ -27,9 +27,10 @@ setup(
     packages=find_packages(exclude=['*.tests']),
     install_requires=[
         'gdal>=1.8.0',
+        'GeoAlchemy2>=0.2.1',
         'pandas>=0.13.1',
         'psycopg2>=2.5',
-
+        'SQLAlchemy>=0.8'
     ],
     extras_require={
         'rastertoolz': ['numpy>=1.8.0', 'rasterio>=0.12', 'rasterstats>=0.4',
