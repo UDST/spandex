60238e607406c109874fdc376403368dc8da5ad3
Dara Adib <dadib@synthicity.com>
2014-09-22 18:06:51 -0700

    Recreate sample schema before each test
    This is slower than an external transaction which is rolled back.
    It also prevents tests from running in parallel. However, it
    doesn't require changes in the way the database class refreshes.
    
    We may wish to revisit this later to speed up tests.

---
+++ spandex/tests/conftest.py
@@ -0,0 +1,39 @@
+import os
+
+import pytest
+
+from spandex.utils import DataLoader
+
+
+@pytest.fixture(scope='function', autouse=True)
+def loader(request):
+    """Recreate sample schema from shapefiles and tear down when done."""
+    # Configure DataLoader to use data directory containing sample shapefiles.
+    root_path = os.path.dirname(__file__)
+    data_path = os.path.join(root_path, '../../test_data')
+    loader = DataLoader(directory=data_path)
+
+    # Recreate PostgreSQL sample schema.
+    with loader.database.cursor() as cur:
+        cur.execute("""
+            CREATE EXTENSION IF NOT EXISTS postgis;
+            DROP SCHEMA IF EXISTS sample CASCADE;
+            CREATE SCHEMA sample;
+        """)
+    loader.database.refresh()
+
+    # Load all shapefiles in test data directory.
+    for filename in os.listdir(data_path):
+        file_root, file_ext = os.path.splitext(filename)
+        if file_ext.lower() == '.shp':
+            shp_path = os.path.join(data_path, filename)
+            table_name = 'sample.' + file_root
+            loader.load_shp(shp_path, table_name)
+
+    # Tear down sample schema when done.
+    def teardown():
+        with loader.database.cursor() as cur:
+            cur.execute("DROP SCHEMA sample CASCADE;")
+    request.addfinalizer(teardown)
+
+    return loader
