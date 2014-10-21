7a33989e82f7977af4732175b5db86f7d1905d0d
Matt Davis <jiffyclub@gmail.com>
2014-10-21 16:54:22 -0700

    Higher level scale_to_targets function
    This takes the whole DataFrame and sequences of targets and filters.
    Clipping and int_result arguments are the same for all segments.

--- spandex/targets.py
+++ spandex/targets.py
@@ -8,6 +8,56 @@ from __future__ import division, print_function
 import pandas as pd
 
 
+def apply_filter_query(df, filters=None):
+    """
+    Use the DataFrame.query method to filter a table down to the
+    desired rows.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+    filters : list of str or str, optional
+        List of filters to apply. Will be joined together with
+        ' and ' and passed to DataFrame.query. A string will be passed
+        straight to DataFrame.query.
+        If not supplied no filtering will be done.
+
+    Returns
+    -------
+    filtered_df : pandas.DataFrame
+
+    """
+    if filters:
+        if isinstance(filters, str):
+            query = filters
+        else:
+            query = ' and '.join(filters)
+        return df.query(query)
+    else:
+        return df
+
+
+def update_series(s1, s2):
+    """
+    Update values in one Series from another Series without modifying either.
+
+    Parameters
+    ----------
+    s1, s2 : pandas.Series
+
+    Returns
+    -------
+    pandas.Series
+
+    """
+    s = s1.astype(s2.dtype)
+
+    for k, v in s2.iteritems():
+        s[k] = v
+
+    return s
+
+
 def _scale_col_to_target(col, target, metric_func):
     """
     Scale a column's values so that in aggregate they match some metric,
@@ -64,3 +114,55 @@ def scale_col_to_target(
         scaled = scaled.round().astype('int')
 
     return scaled
+
+
+def scale_to_targets(
+        df, target_col, targets, metric='mean', filters=None, clip_low=None,
+        clip_high=None, int_result=None):
+    """
+    Parameters
+    ----------
+    df : pandas.DataFrame
+    target_col : str
+        Column in `df` that will be scaled.
+    targets : sequence
+        Sequence of target values. Each target will correspond to a
+        different segment identified by `filters`.
+    metric : {'mean', 'median', 'sum'}
+        How to aggregate the values for comparison to targets.
+    filters : sequence, optional
+        Filters will be used with DataFrame.query and `df` to make a subset of
+        the full table for each scaling operation. Should be the same
+        length as `targets`.
+        Each individual filter can be a string or a sequence of strings.
+        Use ``None`` for no filtering.
+    clip_low : number, optional
+    clip_high : number, optional
+        Bounds for truncating results.
+    int_result : bool, optional
+        Whether result should be rounded and converted to integers.
+
+    Returns
+    -------
+    pandas.DataFrame
+        New DataFrame with `target_col` updated.
+
+    """
+    filters = filters or [None] * len(targets)
+
+    scaled = []
+
+    for t, f, in zip(targets, filters):
+        series = apply_filter_query(df, f)[target_col]
+        scaled.append(scale_col_to_target(series, t, metric))
+
+    scaled = update_series(df[target_col], pd.concat(scaled))
+    scaled = scaled.clip(clip_low, clip_high)
+
+    if int_result:
+        scaled = scaled.round().astype('int')
+
+    df = df.copy()
+    df[target_col] = scaled
+
+    return df
--- spandex/tests/test_targets.py
+++ spandex/tests/test_targets.py
@@ -2,12 +2,39 @@ import pandas as pd
 import pandas.util.testing as pdt
 import pytest
 
-from spandex import targets
+from spandex import targets as tgts
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def col():
     return pd.Series([1, 2, 3, 4, 5])
+
+
+@pytest.fixture(scope='module')
+def target_col():
+    return 'target_col'
+
+
+@pytest.fixture(scope='module')
+def df(target_col):
+    #    a  b  a  b  a  b  a  b  a   b
+    l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    return pd.DataFrame(
+        {target_col: l,
+         'geo_id': ['a', 'b'] * 5,
+         'filter_col': [x + 100 for x in l]})
+
+
+def test_update_series(col):
+    col_copy = col.copy()
+    s2 = pd.Series([9, 99], index=[2, 4])
+
+    result = tgts.update_series(col, s2)
+
+    pdt.assert_series_equal(col, col_copy)
+    pdt.assert_series_equal(
+        result,
+        pd.Series([1, 2, 9, 4, 99]))
 
 
 @pytest.mark.parametrize('metric', ['mean', 'median'])
@@ -15,7 +42,7 @@ def test_scale_col_to_target_mean_median(col, metric):
     target = 600
     expected = pd.Series([200, 400, 600, 800, 1000])
 
-    result = targets.scale_col_to_target(col, target, metric=metric)
+    result = tgts.scale_col_to_target(col, target, metric=metric)
 
     assert getattr(result, metric)() == target
     pdt.assert_series_equal(result, expected, check_dtype=False)
@@ -25,7 +52,7 @@ def test_scale_col_to_target_sum(col):
     target = 16
     expected = col * target / col.sum()
 
-    result = targets.scale_col_to_target(col, target, metric='sum')
+    result = tgts.scale_col_to_target(col, target, metric='sum')
 
     assert result.sum() == target
     pdt.assert_series_equal(result, expected)
@@ -37,7 +64,7 @@ def test_scale_col_to_target_clip(col):
     clip_high = 999
     expected = pd.Series([450, 450, 600, 800, 999])
 
-    result = targets.scale_col_to_target(
+    result = tgts.scale_col_to_target(
         col, target, metric='mean', clip_low=clip_low, clip_high=clip_high)
 
     pdt.assert_series_equal(result, expected, check_dtype=False)
@@ -46,7 +73,56 @@ def test_scale_col_to_target_clip(col):
 def test_scale_col_to_target_round(col):
     target = 16
 
-    result = targets.scale_col_to_target(
+    result = tgts.scale_col_to_target(
         col, target, metric='sum', int_result=True)
 
     pdt.assert_series_equal(result, col)
+
+
+def test_scale_to_targets(df, target_col):
+    targets = [100, 1000]
+    filters = [['geo_id == "a"', 'filter_col < 106'], 'geo_id == "b"']
+    metric = 'sum'
+
+    result = tgts.scale_to_targets(df, target_col, targets, metric, filters)
+
+    pdt.assert_index_equal(result.columns, df.columns)
+    pdt.assert_series_equal(
+        result[target_col],
+        pd.Series(
+            [11.11111111, 66.66666667, 33.33333333, 133.33333333, 55.55555556,
+             200, 7, 266.66666667, 9, 333.33333333]),
+        check_dtype=False)
+
+
+def test_scale_to_targets_no_segments(df, target_col):
+    target = [1000]
+    metric = 'mean'
+
+    result = tgts.scale_to_targets(df, target_col, target, metric=metric)
+
+    pdt.assert_index_equal(result.columns, df.columns)
+    pdt.assert_series_equal(
+        result[target_col],
+        pd.Series(
+            [181.81818182, 363.63636364, 545.45454545, 727.27272727,
+             909.09090909, 1090.90909091, 1272.72727273, 1454.54545455,
+             1636.36363636, 1818.18181818]),
+        check_dtype=False)
+
+
+def test_scale_to_targets_clip_int(df, target_col):
+    target = [1000]
+    metric = 'mean'
+    clip_low = 400
+    clip_high = 999.99
+    int_result = True
+
+    result = tgts.scale_to_targets(
+        df, target_col, target, metric, clip_low=clip_low, clip_high=clip_high,
+        int_result=int_result)
+
+    pdt.assert_index_equal(result.columns, df.columns)
+    pdt.assert_series_equal(
+        result[target_col],
+        pd.Series([400, 400, 545, 727, 909, 1000, 1000, 1000, 1000, 1000]))
