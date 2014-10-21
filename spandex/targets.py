687af1c343c89f839ed83939e62f58ff6b7a5dba
Matt Davis <jiffyclub@gmail.com>
2014-10-21 11:29:12 -0700

    scale_col_to_target function with multi-metric support
    Supports scaling to meet aggregate mean, median, or sum,
    with optional clipping and rounding

--- spandex/targets.py
+++ spandex/targets.py
@@ -5,21 +5,62 @@ match targets.
 """
 from __future__ import division, print_function
 
+import pandas as pd
 
-def scale_col_to_target_mean(col, target):
+
+def _scale_col_to_target(col, target, metric_func):
     """
-    Scale a column's values so that their mean matches a target.
+    Scale a column's values so that in aggregate they match some metric,
+    for example, mean, median, or sum.
 
     Parameters
     ----------
     col : pandas.Series
-    target : float
+    target : number
+    metric_func : callable
+        Must accept a Series and return a number.
 
     Returns
     -------
     scaled : pandas.Series
 
     """
-    current_mean = col.mean()
-    multiplier = target / current_mean
+    current = metric_func(col)
+    multiplier = target / current
     return col * multiplier
+
+
+def scale_col_to_target(
+        col, target, metric='mean', clip_low=None, clip_high=None,
+        int_result=False):
+    """
+    Scale a column's values so they match a target aggregate metric.
+
+    Parameters
+    ----------
+    col : pandas.Series
+    target : number
+    metric : {'mean', 'median', 'sum'}
+        How to aggregate the values in `col` for comparison to `target`.
+    clip_low : number, optional
+    clip_high : number, optional
+    int_result : bool, optional
+        If True, results will be rounded and converted to integers.
+
+    """
+    if metric == 'mean':
+        metric_func = pd.Series.mean
+    elif metric == 'sum':
+        metric_func = pd.Series.sum
+    elif metric == 'median':
+        metric_func = pd.Series.median
+    else:
+        raise ValueError('Unknown metric type: {!r}'.format(metric))
+
+    scaled = _scale_col_to_target(col, target, metric_func)
+    scaled = scaled.clip(clip_low, clip_high)
+
+    if int_result:
+        scaled = scaled.round().astype('int')
+
+    return scaled
--- spandex/tests/test_targets.py
+++ spandex/tests/test_targets.py
@@ -1,12 +1,52 @@
 import pandas as pd
 import pandas.util.testing as pdt
+import pytest
 
 from spandex import targets
 
 
-def test_scale_col_to_target_mean():
-    col = pd.Series([1, 2, 3, 4, 5])
-    result = targets.scale_col_to_target_mean(col, 600)
-    assert result.mean() == 600
-    pdt.assert_series_equal(
-        result, pd.Series([200, 400, 600, 800, 1000]), check_dtype=False)
+@pytest.fixture
+def col():
+    return pd.Series([1, 2, 3, 4, 5])
+
+
+@pytest.mark.parametrize('metric', ['mean', 'median'])
+def test_scale_col_to_target_mean_median(col, metric):
+    target = 600
+    expected = pd.Series([200, 400, 600, 800, 1000])
+
+    result = targets.scale_col_to_target(col, target, metric=metric)
+
+    assert getattr(result, metric)() == target
+    pdt.assert_series_equal(result, expected, check_dtype=False)
+
+
+def test_scale_col_to_target_sum(col):
+    target = 16
+    expected = col * target / col.sum()
+
+    result = targets.scale_col_to_target(col, target, metric='sum')
+
+    assert result.sum() == target
+    pdt.assert_series_equal(result, expected)
+
+
+def test_scale_col_to_target_clip(col):
+    target = 600
+    clip_low = 450
+    clip_high = 999
+    expected = pd.Series([450, 450, 600, 800, 999])
+
+    result = targets.scale_col_to_target(
+        col, target, metric='mean', clip_low=clip_low, clip_high=clip_high)
+
+    pdt.assert_series_equal(result, expected, check_dtype=False)
+
+
+def test_scale_col_to_target_round(col):
+    target = 16
+
+    result = targets.scale_col_to_target(
+        col, target, metric='sum', int_result=True)
+
+    pdt.assert_series_equal(result, col)
